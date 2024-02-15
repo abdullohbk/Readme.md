ab8acbed78e163b3f9e230e21227359fcc1c4842
abdullohbk <tursunovabdulloh858@gmail.com>
2024-02-15 20:22:29 +0500

    plus filter and map functions

---
+++ script.js
@@ -0,0 +1,52 @@
+// function People(data) {
+//   return data.map((a) => a.price);
+// }
+
+// const result = People(product.products);
+// console.log(result);
+
+////////////////////////////////////////////////////////////
+
+// function filterPopulation(data) {
+//   return data.filter((el) => el.brand == "Apple");
+// }
+
+// let result = filterPopulation(product.products);
+// console.log(result);
+
+///////////////////////////////////////////////////////////////
+
+// function filterPopulation(data) {
+//   return data.filter((el) => el.category == "smartphones");
+// }
+
+// let result = filterPopulation(product.products);
+// console.log(result);
+
+////////////////////////////////////////////////////////
+
+// function filterPopulation(data) {
+//   return data.filter((el) => el.rating >= 4.6);
+// }
+
+// let result = filterPopulation(product.products);
+// console.log(result);
+
+
+/////////////////////////////////////////////////
+
+// function MakeObject(array) {
+//   return array.map((el) => {
+//     return {
+//       nomi: `${el.title}`,
+//       narxi: `${el.price}`,
+//       reyting: `${el.rating}`,
+//       kategoriya: `${el.category}`,
+//       rasmi: `${el.thumbnail}`,
+//     };
+//   });
+// }
+
+// const result = MakeObject(product.products);
+
+// console.log(result);
